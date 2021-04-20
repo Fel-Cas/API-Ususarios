8ee58b302f52a861ed06a11085b0f97517d2c23e
Andrés F. Castro <felimon0619@hotmail.com>
2021-04-19 19:56:00 -0500

    Rutas

--- src/routes/usuario.js
+++ src/routes/usuario.js
@@ -4,8 +4,8 @@ const router=express.Router();
 const usuarioController=require('../controllers/usuarioController');
 const token=require('../middleware/token');
 
-router.get('/',usuarioController.obtenerUsuarios);
-router.get('/:username',usuarioController.obtenerUsuario);
+router.get('/',token.validartoken,usuarioController.obtenerUsuarios);
+router.get('/:username',token.validartoken,usuarioController.obtenerUsuario);
 router.post('/',usuarioController.crearUsuario);
 router.put('/:username',token.validartoken,usuarioController.actualizarUsuario);
 router.delete('/:username',token.validartoken,usuarioController.eliminarUsuario);
